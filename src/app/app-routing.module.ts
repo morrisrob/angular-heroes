f07658ec5d827bcc47370b602ab7eeebab4e69d0
morrisrobert <morrisrobert@outlook.com>
2019-12-09 12:13:37 -0600

    initial commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { HeroDetailsComponent } from "./hero-details/hero-details.component";
+import { DashboardComponent } from "./dashboard/dashboard.component";
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { HeroesComponent } from "./heroes/heroes.component";
+
+const routes: Routes = [
+  { path: "", redirectTo: "/dashboard", pathMatch: "full" },
+  { path: "heroes", component: HeroesComponent },
+  { path: "dashboard", component: DashboardComponent },
+  { path: "detail/:id", component: HeroDetailsComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
